ed47972daad559aea8e749651a8abe31eb1fc6b2
Marcelle Chiriboga <marcelle.chiriboga@gmail.com>
2019-02-08 20:40:10 -0800

    Add functions documentation

--- audiofilters/reverb.py
+++ audiofilters/reverb.py
@@ -1,2 +1,18 @@
 def add_reverb(input_signal, type = 'hall'):
+    """Add a reverb effect to the audio signal, simulating a different recording environment
+
+    Parameters
+    ----------
+    input_signal : numpy.array
+        Input array, must have float32 type.
+    type : string
+        Choice of reverb effect. 
+        Options are 'hall' and 'church'. 
+        Defaults to 'hall'
+
+    Returns
+    -------
+    numpy.array representing the audio signal with the specified type of reverb applied
+        
+    """
     return []
