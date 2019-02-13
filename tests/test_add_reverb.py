d12a6d803b94e9edae8171d8578b1559d8c344ac
Marcelle Chiriboga <marcelle.chiriboga@gmail.com>
2019-02-12 20:38:49 -0800

    Fix file paths in test_add_reverb

--- tests/test_add_reverb.py
+++ tests/test_add_reverb.py
@@ -2,16 +2,16 @@ import numpy as np
 import pytest
 from audiofilters.reverb import add_reverb
 
-input_signal = np.genfromtxt('data/bark.csv', dtype = 'float32')
+input_signal = np.genfromtxt('tests/data/bark.csv', dtype = 'float32')
 
 def test_hall_reverb_is_applied_correctly():
-    expected_output = np.genfromtxt('data/reverb/bark_hall.csv', dtype = 'float32')
+    expected_output = np.genfromtxt('tests/data/reverb/bark_hall.csv', dtype = 'float32')
     output_signal = add_reverb(input_signal, 'hall')
 
     assert np.array_equal(output_signal, expected_output)
-    
+
 def test_church_reverb_is_applied_correctly():
-    expected_output = np.genfromtxt('data/reverb/bark_church.csv', dtype = 'float32')
+    expected_output = np.genfromtxt('tests/data/reverb/bark_church.csv', dtype = 'float32')
     output_signal = add_reverb(input_signal, 'church')
 
     assert np.array_equal(output_signal, expected_output)
