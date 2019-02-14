73806b260b4b759dcbd2fd7cd79350faa753b918
Paul Vial <vial.paul@gmail.com>
2019-02-13 20:06:28 -0800

    Lowpass test changes
    Now uses mean squared error to compare instead of requiring exact match.

--- tests/test_low_pass_filter.py
+++ tests/test_low_pass_filter.py
@@ -10,4 +10,7 @@ def test_low_pass_filter_attenuates_frequencies_below_1000_cutoff():
     expected_output = np.genfromtxt('data/lowpass/bark_lowpass_1000Hz_24db.csv', dtype = 'float32')
     output_signal = low_pass_filter(input_signal, 1000)
 
-    assert np.array_equal(output_signal, expected_output), "Output does not match expected output!"
+    # Mean squared error between input and output signal
+    mse = ((expected_output - output_signal)**2).mean(axis=0)
+
+    assert mse < 0.01, "Output does not match test data!"
